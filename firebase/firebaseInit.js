745f047c7c4d899e9e6b9ef2b42f7484077e36bc
Immortal000 <kotthursiddarth@gmail.com>
2021-04-04 19:47:55 -0500

    first commit

--- firebase/firebaseInit.js
+++ firebase/firebaseInit.js
@@ -1,24 +1,24 @@
-import firebase from "firebase/app"
+import firebase from "firebase/app";
 
-import "firebase/auth"
-import "firebase/firestore"
-import "firebase/storage"
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
-export default function firebaseInit(){
-    const firebaseConfig = {
-        apiKey: "AIzaSyC9aj7Xlno0hDnLu59OgLiMDjD7z97Yiug",
-        authDomain: "fire0base145.firebaseapp.com",
-        databaseURL: "https://fire0base145.firebaseio.com",
-        projectId: "fire0base145",
-        storageBucket: "fire0base145.appspot.com",
-        messagingSenderId: "769441970390",
-        appId: "1:769441970390:web:f6d1f20667dcd030a27177"
-    };
+export default function firebaseInit() {
+  const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+  };
 
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-        console.log("Firebase Connected!")
-    }else {
-        firebase.app();
-    }
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+    console.log("Firebase Connected!");
+  } else {
+    firebase.app();
+  }
 }
